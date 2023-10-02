5375fe1e419c4c23d32569941a55a23a37acd864
phoebegrace <phoebegrace.juayong@wvsu.edu.ph>
2023-10-02 11:54:26 +0800

    log in dart update

---
+++ lib/features/log_in.dart
@@ -0,0 +1,13 @@
+mport 'package:flutter/material.dart';
+
+class login extends StatelessWidget {
+  const login ({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      Text("Login");
+      
+    );
+  }
+}
