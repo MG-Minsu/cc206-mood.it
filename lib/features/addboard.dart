38a5a88a0f9947c0ea2d0727efc77b83efd19a16
Mathew Gabriel <99707207+MG-Minsu@users.noreply.github.com>
2023-10-02 11:22:47 +0800

    dart file update

---
+++ lib/features/addboard.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class addboard extends StatelessWidget {
+  const addboard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      Text("Add Board");
+      
+    );
+  }
+}
