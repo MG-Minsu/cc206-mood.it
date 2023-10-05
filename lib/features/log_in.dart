cdec074648198738e5f00efce005f5e3a91148fe
phoebegrace <phoebegrace.juayong@wvsu.edu.ph>
2023-10-05 15:12:33 +0800

    log_in dart update

--- lib/features/log_in.dart
+++ lib/features/log_in.dart
@@ -1,13 +1,56 @@
-mport 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 
-class login extends StatelessWidget {
-  const login ({super.key});
+void main() {
+  runApp(LoginApp());
+}
 
+class LoginApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      Text("Login");
-      
+    return MaterialApp(
+      home: LoginPage(),
+    );
+  }
+}
+
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Login Page'),
+      ),
+      body: Center(
+        child: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              TextField(
+                decoration: InputDecoration(
+                  hintText: 'username',
+                ),
+              ),
+              SizedBox(height: 16.0),
+              TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  hintText: 'password',
+                ),
+              ),
+              SizedBox(height: 16.0),
+              ElevatedButton(
+                onPressed: () {
+                  // Add your login logic here
+                  // For simplicity, we're not handling authentication in this example.
+                  // You would typically validate the entered username and password.
+                },
+                child: Text('Login'),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
