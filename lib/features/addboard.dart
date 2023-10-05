545a56556ba55f8385df4e151105b35979adde65
Mathew Gabriel <99707207+MG-Minsu@users.noreply.github.com>
2023-10-05 14:53:52 +0800

    feature update in addboard

--- lib/features/addboard.dart
+++ lib/features/addboard.dart
@@ -1,65 +1,47 @@
 import 'package:flutter/material.dart';
 
 class AddBoard extends StatelessWidget {
-  const AddBoard({Key? key});
+  const AddBoard({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          // Add new board
-          margin: EdgeInsets.fromLTRB(0, 0, 234, 4.58),
-          child: Text(
-            'Add New Board',
-            style: TextStyle(
-              fontFamily: 'Poppins',
-              fontSize: 14,
-              fontWeight: FontWeight.w400,
-              height: 1.5,
-              color: Color(0xff000000),
-            ),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "Add New Board",
+          style: TextStyle(
+            color: Colors.white, // Title text color
           ),
         ),
-        TextField(
-          decoration: InputDecoration(labelText: 'Enter Title'),
+        leading: const Icon(
+          Icons.clear, // This is the "X" icon
+          size: 32.0, // Icon size in the AppBar
+          color: Colors.white, // Icon color
         ),
-        TextField(
-          decoration: InputDecoration(labelText: 'Enter Text'),
-        ),
-        TextButton(
-          // sidebarexit6LV (31:277)
-          onPressed: () {},
-          style: TextButton.styleFrom(
-            padding: EdgeInsets.zero,
-          ),
-          child: Container(
-            width: 36.77,
-            height: 39.58,
-            child: Image.network(
-              '[Image URL]',
-              width: 36.77,
-              height: 39.58,
-            ),
-          ),
-        ),
-        Positioned(
-          // imagefill0wght400grad0opsz2411 (78:421)
-          left: 358.5,
-          top: 240,
-          child: Align(
-            child: SizedBox(
-              width: 18,
-              height: 18,
-              child: Image.network(
-                '[Image URL]',
-                width: 18,
-                height: 18,
+        backgroundColor: Color.fromARGB(255, 4, 102, 144), // App bar background color
+      ),
+      body: const Padding(
+        padding: const EdgeInsets.all(16.0), // Add padding to the entire body
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start, // Align content to the left
+          children: [
+            SizedBox(height: 20), // Add some spacing below the AppBar
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'Enter Title',
+                border: OutlineInputBorder(), // Add a border to the text field
               ),
             ),
-          ),
+            SizedBox(height: 16), // Add spacing between text fields
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'Enter Mood content',
+                border: OutlineInputBorder(), // Add a border to the text field
+              ),
+            ),
+          ],
         ),
-      ],
+      ),
     );
   }
 }
