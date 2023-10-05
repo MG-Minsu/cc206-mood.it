012d9db3aaab1c3d71a02fb155fd07262c4227f0
Mathew Gabriel <99707207+MG-Minsu@users.noreply.github.com>
2023-10-05 15:14:47 +0800

    addboard dart update

--- lib/features/addboard.dart
+++ lib/features/addboard.dart
@@ -20,24 +20,33 @@ class AddBoard extends StatelessWidget {
         ),
         backgroundColor: Color.fromARGB(255, 4, 102, 144), // App bar background color
       ),
-      body: const Padding(
+      body: Padding(
         padding: const EdgeInsets.all(16.0), // Add padding to the entire body
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start, // Align content to the left
           children: [
-            SizedBox(height: 20), // Add some spacing below the AppBar
-            TextField(
+            const SizedBox(height: 20), // Add some spacing below the AppBar
+            const TextField(
               decoration: InputDecoration(
                 labelText: 'Enter Title',
                 border: OutlineInputBorder(), // Add a border to the text field
               ),
             ),
-            SizedBox(height: 16), // Add spacing between text fields
-            TextField(
+            const SizedBox(height: 16), // Add spacing between text fields
+            const TextField(
               decoration: InputDecoration(
                 labelText: 'Enter Mood content',
                 border: OutlineInputBorder(), // Add a border to the text field
               ),
+            ),
+            const SizedBox(height: 16.0),
+            ElevatedButton(
+              onPressed: () {
+                // Add your login logic here
+                // For simplicity, we're not handling authentication in this example.
+                // You would typically validate the entered username and password.
+              },
+              child: const Text('Save'),
             ),
           ],
         ),
