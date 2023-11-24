dd5832418fa8f1869578b8f0b243072c605da386
phoebegrace <phoebegrace.juayong@wvsu.edu.ph>
2023-11-24 19:58:14 +0800

    Login and About us page update

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+
 import 'package:flutter/material.dart';
-import 'features/log_in.dart';
+import 'components/features/log_in.dart';
+
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'mood.it',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -29,10 +31,28 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 0, 0, 0)),
         useMaterial3: true,
+        
       ),
-      home: LoginApp(),
+      
+      home: const Scaffold(
+        body: NewWidget(),
+        drawer: MyDrawer(),
+      ),
+      
+      debugShowCheckedModeBanner: false,
     );
+  }
+}
+
+class NewWidget extends StatelessWidget {
+  const NewWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return LoginApp();
   }
 }
